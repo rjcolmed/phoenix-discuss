b67738d89d972974eccb4677f37560da1ef21859
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-06-05 19:09:46 -0400

    add changeset variable and call render with new.html template in #new

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,13 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, params) do
-  
+    changeset = Discuss.Topic.changeset(%Topic{}, %{})
+
+    #By default, conn is always passed to templates
+    render conn, "new.html", changeset: changeset
   end
+
 end
