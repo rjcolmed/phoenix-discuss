630e7019c88b8a1b675e3e9af3a3ec5f30b58979
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-06-05 20:03:13 -0400

    ignore params in #new, add #create action

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,11 +3,15 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Discuss.Topic.changeset(%Topic{}, %{})
 
     #By default, conn is always passed to templates
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{ "topic" => topic }) do
+
   end
 
 end
